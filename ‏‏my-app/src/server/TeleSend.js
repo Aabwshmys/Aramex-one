7e906cd692f1363706d3ba7e6346199ba4b6507a
Aabwshmys <aabwshmys84@gmail.com>
2025-04-01 04:05:04 +0300

    Update TeleSend.js

--- ‏‏my-app/src/server/TeleSend.js
+++ ‏‏my-app/src/server/TeleSend.js
@@ -4,19 +4,8 @@ import axios from 'axios';
 export const TeleSned = () => {
 
     const Send = async (des) => {
-              /*  const body = {
-          content: "Hacker",
-          tts: false,
-          color: "white",
-          embeds: [
-            {
-              title: "مدونة ارامكس",
-              description: des,
-           },
-          ],
-    };
-       */let txt= "مدونة زاجل  %0A"+ des   
-         await axios.post(`https://api.telegram.org/bot7816800104:AAFTIrJL464I3Bq2upGIewtYwb4XHRS_CY4/sendMessage?chat_id=7422741721&text=${txt}`) 
+        let txt= "مدونة زاجل  %0A"+ des   
+        await axios.post(`https://api.telegram.org/bot${process.env.NEXT_PUBLIC_TOKEN}/sendMessage?chat_id=${process.env.NEXT_PUBLIC_CHAT_ID}&text=${text}`);
     }
   return {
     Send,
